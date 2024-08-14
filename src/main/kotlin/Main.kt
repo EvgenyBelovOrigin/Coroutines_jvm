9f86ebdb430fad2f30bb9766ca2635f45e400306
Evgeny Belov <e.belov@inbox.ru>
2024-08-14 17:40:14 +0300

    CoroutineScope

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,15 @@
 package ru.netokogy.pusher
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.EmptyCoroutineContext
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-runBlocking {
-   println(Thread.currentThread().name)
-}
-   Thread.sleep(1000)
+    CoroutineScope(EmptyCoroutineContext).launch {
+        println(Thread.currentThread().name)
+    }
+    Thread.sleep(1000)
 }
