c25cbc0c100960630290c8469c6651583e478d3d
Evgeny Belov <e.belov@inbox.ru>
2024-08-14 17:29:50 +0300

    runBlocking

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
 package ru.netokogy.pusher
 
+import kotlinx.coroutines.runBlocking
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-   println("Hello")
+runBlocking {
+   println(Thread.currentThread().name)
+}
+   Thread.sleep(1000)
 }
