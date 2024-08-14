b6b0d41d946083d55a18ff4feb5e8d8fcaeefc7c
Evgeny Belov <e.belov@inbox.ru>
2024-08-14 18:34:17 +0300

    CoroutineScope&Dispatchers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,33 @@
 package ru.netokogy.pusher
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import java.util.concurrent.Executors
 import kotlin.coroutines.EmptyCoroutineContext
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+//1111111111111111111111111111111111111111111111111111111111111
     CoroutineScope(EmptyCoroutineContext).launch {
         println(Thread.currentThread().name)
     }
+    Thread.sleep(1000L)
+
+println()
+//2222222222222222222222222222222222222222222222222222222222
+    val custom = Executors.newFixedThreadPool(64).asCoroutineDispatcher()
+    with (CoroutineScope(EmptyCoroutineContext)) {
+        launch(Dispatchers.Default) {
+            println(Thread.currentThread().name)
+        }
+        launch(Dispatchers.IO) {
+            println(Thread.currentThread().name)
+        }
+        launch(custom) {
+            println(Thread.currentThread().name)
+        }
+    }
     Thread.sleep(1000)
+    custom.close()
+
 }
