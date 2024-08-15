885b68509a79a9356d85f3d57ef8b85bd9c830e0
Evgeny Belov <e.belov@inbox.ru>
2024-08-15 19:32:23 +0300

    async await

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,92 @@
 package ru.netokogy.pusher
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.*
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import okio.Timeout
+import ru.netokogy.pusher.dto.Comment
+import ru.netokogy.pusher.dto.Post
+import ru.netokogy.pusher.dto.PostWithComments
+import java.io.IOException
+import java.lang.RuntimeException
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+private const val BASE_URL = "http://192.168.1.36:9999"
+private val gson = Gson()
+
+private val client = OkHttpClient.Builder()
+    .addInterceptor(HttpLoggingInterceptor(::println).apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    })
+    .connectTimeout(30, TimeUnit.SECONDS)
+    .build()
+
+
 fun main() {
-//1111111111111111111111111111111111111111111111111111111111111
-    CoroutineScope(EmptyCoroutineContext).launch {
-        println(Thread.currentThread().name)
-    }
-    Thread.sleep(1000L)
-
-println()
-//2222222222222222222222222222222222222222222222222222222222
-    val custom = Executors.newFixedThreadPool(64).asCoroutineDispatcher()
-    with (CoroutineScope(EmptyCoroutineContext)) {
-        launch(Dispatchers.Default) {
-            println(Thread.currentThread().name)
-        }
-        launch(Dispatchers.IO) {
-            println(Thread.currentThread().name)
-        }
-        launch(custom) {
-            println(Thread.currentThread().name)
+    with(CoroutineScope(EmptyCoroutineContext)) {
+        launch {
+            try {
+                val posts = getPosts(client)
+                    .map { post ->
+                        async {
+                            PostWithComments(post, getComments(client, post.id))
+                        }
+                    }.awaitAll()
+                println(posts)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
-    Thread.sleep(1000)
-    custom.close()
+    Thread.sleep(3000_000L)
 
 }
+
+suspend fun OkHttpClient.apiCall(url: String): Response {
+    return suspendCoroutine { continuation ->
+        Request.Builder()
+            .url(url)
+            .build()
+            .let(::newCall)
+            .enqueue(object : Callback {
+
+                override fun onResponse(call: Call, response: Response) {
+                    continuation.resume(response)
+                }
+
+                override fun onFailure(call: Call, e: IOException) {
+                    continuation.resumeWithException(e)
+                }
+
+
+            })
+
+    }
+}
+
+suspend fun <T> makeRequest(url: String, client: OkHttpClient, typeToken: TypeToken<T>): T =
+    withContext(Dispatchers.IO) {
+        client.apiCall(url)
+            .let { response ->
+                if (!response.isSuccessful) {
+                    response.close()
+                    throw RuntimeException(response.message)
+                }
+                val body = response.body ?: throw RuntimeException("response body is null")
+                gson.fromJson(body.string(), typeToken.type)
+            }
+    }
+
+suspend fun getPosts(client: OkHttpClient): List<Post> =
+    makeRequest("$BASE_URL/api/slow/posts", client, object : TypeToken<List<Post>>() {})
+
+suspend fun getComments(client: OkHttpClient, id: Long): List<Comment> =
+    makeRequest("$BASE_URL/api/slow/posts/$id/comments", client, object : TypeToken<List<Comment>>() {})
